b83dfcb54d8facb3150890b1c911e9ac44c11d25
Altono1 <altonobinna11@gmail.com>
2023-11-08 19:13:33 +0100

    v5

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
     images: {
         domains: ['firebasestorage.googleapis.com', 'lh3.googleusercontent.com']
     }
