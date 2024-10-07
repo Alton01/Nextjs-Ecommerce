33dfbab36ffe9a4011dc67f2fecb52306cdade9a
Altono1 <altonobinna11@gmail.com>
2024-10-07 09:54:43 +0100

    protect summary route1

--- app/order/[orderId]/OrderItem.tsx
+++ app/order/[orderId]/OrderItem.tsx
@@ -13,7 +13,7 @@ const OrderItem: React.FC<OrderItemProps> = ({ item }) => {
   return (
     <div className="grid grid-cols-5 text-xs md:text-sm gap-4 border-t-[1.5px] border-slate-200 py-4 items-center">
       <div className="col-span-2 justify-self-start flex gap-2 md:gap-4">
-        <div className="relative w-[70px] aspect-square">
+        <div className="relative w-[70px] aspect-square hidden sm:block">
           <Image
             src={item.selectedImg.image}
             alt={item.name}
