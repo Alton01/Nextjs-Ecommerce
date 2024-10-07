6b642f6ad20be5423cce3a92ec28609fe2cf9964
Altono1 <altonobinna11@gmail.com>
2024-10-07 09:46:40 +0100

    protect summary route

--- actions/getUsers.ts
+++ actions/getUsers.ts
@@ -1,11 +1,11 @@
-import prisma from '@/libs/prismadb'
+import prisma from "@/libs/prismadb";
 
 export default async function getUsers() {
-    try {
-       const users = prisma?.user.findMany()
-       
-       return users
-    } catch (error: any) {
-        throw new  Error(error)
-    }
+  try {
+    const users = await prisma?.user.findMany();
+
+    return users;
+  } catch (error: any) {
+    throw new Error(error);
+  }
 }
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -5,12 +5,19 @@ import getUsers from "@/actions/getUsers";
 import Container from "../components/Container";
 import BarGraph from "./BarGraph";
 import getGraphData from "@/actions/getGraphData";
+import { getCurrentUser } from "@/actions/getCurrentUser";
+import NullData from "../components/NullData";
 
 const Admin = async () => {
   const products = await getProducts({ category: null });
   const orders = await getOrders();
   const users = await getUsers();
   const graphData = await getGraphData();
+  const currentUser = await getCurrentUser();
+
+  if (!currentUser || currentUser.role !== "ADMIN") {
+    return <NullData title="OOPS!! YOU ARE NOT ADMIN!! ACCESS DENIED" />;
+  }
 
   return (
     <div className="pt-8">
